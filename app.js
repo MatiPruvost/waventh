93fff0b187ac8b1bb2a017051a94f5be5cb955da
MatiPruvost <matiaspruvost@gmail.com>
2015-04-06 22:29:19 -0300

    Rename file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var util = require('util');
 var colorsLogs = require('colors');
 var CronJob = require('cron').CronJob;
-var twitt = require('./twitt.js');
+var tweet = require('./tweet.js');
 var config = require('./config').cron_job;
 var logs = require('./logs');
 
@@ -11,14 +11,14 @@ colorsLogs.setTheme({
 });
 
 console.log('Initializing waventh'.initializing);
-twitt.create();
+tweet.create();
 console.log('Waiting for tweet'.initializing);
 
 var cronTime_config = util.format('%s */%s * * *', config.minute, config.hour);
 var job = new CronJob({
   cronTime: cronTime_config,
   onTick: function() {
-    twitt.create();
+    tweet.create();
     console.log('Waiting for tweet'.initializing);
   },
 });
